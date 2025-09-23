788a44dc8ab9d2090d5a98bfe382c73b950aafeb
a240265nk <a240265nk@rvt.lv>
2025-09-23 09:54:49 +0300

    Grades and Points

---
+++ src/main/java/rvt/Grades.java
@@ -0,0 +1,30 @@
+package rvt;
+
+import java.util.Scanner;
+
+public class Grades {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Give points [0-100]:");
+        int points = scanner.nextInt();
+        String grade = "0";
+        if (points < 0) {
+            grade = "impossible!";
+        } else if (points <= 49) {
+            grade = "failed";
+        } else if (points <= 59) {
+            grade = "1";
+        } else if (points <= 69) {
+            grade = "2";
+        } else if (points <= 79) {
+            grade = "3";
+        } else if (points <= 89) {
+            grade = "4";
+        } else if (points <= 100) {
+            grade = "5";
+        } else {
+            grade = "incredible!";
+        } 
+        System.out.println("Grade: " + grade);     
+    }
+}
