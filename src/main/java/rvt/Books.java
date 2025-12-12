75c8a8f45c57c149dc38ef212bbcaf53b83f7dae
24DP2NKapa <A240265NK@rvt.lv>
2025-12-12 20:36:23 +0200

    Update Books.java
    Finish Books.java with freshly learned convenient methods

--- src/main/java/rvt/Books.java
+++ src/main/java/rvt/Books.java
@@ -5,31 +5,62 @@ import java.util.*;
 public class Books {
     private String title;
     private int pages;
-    private int publication_year;
-    ArrayList<String> books = new ArrayList<>();
+    private int pub_year;
 
-    public Books() {
+    public Books(String title, int pages, int year) {
         this.title = title;
         this.pages = pages;
-        this.publication_year = publication_year;
+        this.pub_year = year;
     }
-    public void printInfo() {
-        Scanner scanner = new Scanner(System.in);
 
+    public void printAll() {
+        System.out.print(this.title + ", ");
+        System.out.print(this.pages + " pages, ");
+        System.out.print(this.pub_year);
     }
-    public void readFromUser() {
-        Scanner scanner = new Scanner(System.in);
-        while (true) {
-            String userinp = scanner.nextLine();
-            System.out.print("Title: ");
-            //title = ;
-            books.add(userinp);
-            System.out.print("Pages: ");
-            System.out.print("Publication year: ");
-        }
+
+    public void printTitle() {
+        System.out.println(this.title);
     }
 
     public static void main(String[] args) {
-        Books books = new Books();
+        Scanner scanner = new Scanner(System.in);
+        ArrayList<Books> library = new ArrayList<>();
+
+        while (true) {
+            System.out.print("Title: ");
+            String new_title = scanner.nextLine();
+
+            if (new_title.isEmpty()) {
+                System.out.print("What information will be printed? ");
+                String info = scanner.nextLine();
+
+                if (info.equals("name")) {
+                    for (Books b : library) {
+                        b.printTitle();
+                        System.out.println();
+                    }
+                } else if (info.equals("everything")) {
+                    for (Books b : library) {
+                        b.printAll();
+                        System.out.println();
+                    }
+                } else {
+                    System.out.println("Please pick valid option!");
+                }
+
+                break;
+            }
+
+            System.out.print("Pages: ");
+            int new_pages = scanner.nextInt();
+
+            System.out.print("Publication year: ");
+            int new_year = scanner.nextInt();
+
+            scanner.nextLine();
+
+            library.add(new Books(new_title, new_pages, new_year));
+        }
     }
 }
